3bb9b4696abf2749da5c1687a9965d07fcb0282f
vanmtv <vanmtv@hotmail.com>
2020-11-16 12:11:12 -0300

    Add UsuarioService c login e senha criptografada

--- Spring/blogPessoalVan/src/main/java/org/generation/blogPessoalVan/service/UsuarioService.java
+++ Spring/blogPessoalVan/src/main/java/org/generation/blogPessoalVan/service/UsuarioService.java
@@ -18,13 +18,18 @@ public class UsuarioService {
 	@Autowired
 	private UsuarioRepository repository;
 
-	public Usuario CadastrarUsuario(Usuario usuario) {
+	public Optional<Usuario> CadastrarUsuario(Usuario usuario) {
+		
+		
+		if(repository.findByUsuario(usuario.getUsuario()).isPresent())
+			return null;
+		
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
 		String senhaEncoder = encoder.encode(usuario.getSenha());
 		usuario.setSenha(senhaEncoder);
 
-		return repository.save(usuario);
+		return Optional.of(repository.save(usuario));
 	}
 
 	public Optional<UserLogin> Logar(Optional<UserLogin> user) {
@@ -41,6 +46,7 @@ public class UsuarioService {
 
 				user.get().setToken(authHeader);				
 				user.get().setNome(usuario.get().getNome());
+				user.get().setSenha(usuario.get().getSenha());
 
 				return user;
 			}
